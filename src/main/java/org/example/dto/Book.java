61b18366de76d77c280751a8ac2c659bec3e8875
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-01-25 12:17:05 +0530

    first commit

---
+++ src/main/java/org/example/dto/Book.java
@@ -0,0 +1,19 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Book {
+    private String ISBN;
+    private String title;
+    private String author;
+    private String category;
+    private Integer qty;
+
+}
